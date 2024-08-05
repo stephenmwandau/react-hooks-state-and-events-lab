2cec50ec4d629c6ea99ba0c8746ccb7260192f74
stephenmwandau <stephen.mwandau@student.moringaschool.com>
2024-08-05 16:03:25 +0300

    completed

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  // State variable to track if the item is in the cart
+  const [inCart, setInCart] = useState(false);
+
+  // Function to toggle item in and out of cart
+  const handleToggleCart = () => {
+    setInCart(!inCart);
+  };
+
   return (
-    <li className="">
+    <li className={inCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={inCart ? "remove" : "add"} onClick={handleToggleCart}>
+        {inCart ? "Remove From Cart" : "Add to Cart"}
+      </button>
     </li>
   );
 }
